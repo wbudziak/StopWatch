98414aa12ac20cd6213b534faf5fc2ec843146bc
Wojciech Budziak <68879652+wbudziak@users.noreply.github.com>
2020-12-09 22:40:31 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+const btn = document.querySelector(".start");
+const btnStop = document.querySelector(".stop");
+const btnReset = document.querySelector(".reset");
+const btnSave = document.querySelector(".save");
+const scores  = document.querySelector(".scores")
+let minutes = document.querySelector(".minutes");
+let sec = document.querySelector(".sec");
+let mSec =document.querySelector(".mSec");
+let timer = document.querySelector(".timer");
+// minutes.innerText = "00";
+// sec.innerText = "00";
+// timer.innerText = "00";
+let number = 0;
+timeMin = 0;
+timeSec = 0;
+timeMSec =0;
+btn.addEventListener("click",function(){
+    btn.innerText = "Start";    
+    const time = setInterval(() => {
+        timeMSec++;
+        if(timeMSec === 100){
+            timeMSec = 0;
+            timeSec++;
+            if(timeSec === 60){
+                timeSec = 0;
+                timeMin++;
+            }
+        }
+        timer.innerText = `${("0" + timeMin).slice(-2)}:${("0" + timeSec).slice(-2)},${("0" + timeMSec).slice(-2)}`;
+        
+    }, 10);    
+   
+
+    btnStop.addEventListener("click",()=>{
+        clearInterval(time);
+        btn.innerText = "continue";
+    })
+    btnReset.addEventListener("click",()=>{
+        clearInterval(time);
+        timeMSec = 0;
+        timeSec =0;
+        timeMin = 0;
+        timer.innerText = `00:00,00`;
+        time.stop();
+    })
+  
+    
+})
+btnSave.addEventListener("click",()=>{
+    const ScoreBox = document.createElement('div');
+    scores.appendChild(ScoreBox);
+
+    number++;
+    const span = document.createElement('span');
+    span.textContent = `Round ${number}`;
+    ScoreBox.appendChild(span);
+
+    const spanik = document.createElement('span');
+    spanik.textContent = `${("0" + timeMin).slice(-2)}:${("0" + timeSec).slice(-2)},${("0" + timeMSec).slice(-2)}`;
+    ScoreBox.appendChild(spanik);
+
+    setTimeout(() => {
+        span.classList.add("spanOn");
+        spanik.classList.add("spanOn");
+    }, 100);
+    setTimeout(() => {
+        span.classList.remove("spanOn");
+        spanik.classList.remove("spanOn");
+    }, 1250);
+
+
+})
